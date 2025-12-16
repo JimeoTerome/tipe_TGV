97bf07ad1c0869d30b3640c73ba00114138c3949
gagalebig <97346635+gagalebig@users.noreply.github.com>
2025-12-16 12:35:37 +0100

    Update fichier.py

--- interface_et_stats/fichier.py
+++ interface_et_stats/fichier.py
@@ -1,1 +1,9 @@
-
+def afiicher_plateau (plateau):
+  for i in range (0,2):
+    for j in range (0,2):
+      for k in range (0,2):
+        for l in range (0,2):
+          print (plateau[i+k][j+l])
+          print (" ")
+        print("| ")
+      print ("\n")
