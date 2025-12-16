15bc91ce3f53b08b85d5342d856d54e6772f9c0a
gagalebig <97346635+gagalebig@users.noreply.github.com>
2025-12-16 16:32:10 +0100

    Update fichier.py

--- interface_et_stats/fichier.py
+++ interface_et_stats/fichier.py
@@ -1,13 +1,14 @@
-def afiicher_plateau (plateau):
-  for i in range (0,2):
-    for j in range (0,2):
-      for k in range (0,2):
-        for l in range (0,2):
-          if (plateau[i+k][j+l]==1) :
-            print("X")
-          if (plateau[i+k][j+l]==-1) :
-            print("O")
-          else print ("·")
-          print (" ")
-        print("| ")
-      print ("\n")
+def afficher_plateau(plateau):
+  for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("-" * 21)
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print("|", end=" ")
+            if plateau[i][j] == 1:
+                print("X", end=" ")
+            elif plateau[i][j] == -1:
+                print("O", end=" ")
+            else:
+                print("·", end=" ")
+        print()
