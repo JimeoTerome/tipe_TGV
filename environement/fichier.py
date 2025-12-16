3ab25e19260671e63547c15fef7af1afad34eb75
gagalebig <97346635+gagalebig@users.noreply.github.com>
2025-12-16 16:48:09 +0100

    Update fichier.py

--- environement/fichier.py
+++ environement/fichier.py
@@ -5,6 +5,10 @@ import torch.nn as nn # type: ignore
 import torch.optim as optim # type: ignore
 from torch.distributions import Categorical # type: ignore
 
+stat_sous_morpion = [[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0],[0,0,0]] # dans l'ordre de lecture // 1 : X ; 2 : 0 ; 3 : nul
+couleur_IA = 1 # 1 si IA joue les X, 0 sinon
+
+
 class Stictactoe(gym.Env):
 
     def __init__(self):
@@ -67,3 +71,18 @@ def qui_a_gagne(plateau):
     for i in range(9):
         tempo[i] = qui_a_gagne_morpion(plateau[i])
     return qui_a_gagne_morpion(tempo)
+
+def afficher_plateau(plateau):
+  for i in range(9):
+        if i % 3 == 0 and i != 0:
+            print("-" * 21)
+        for j in range(9):
+            if j % 3 == 0 and j != 0:
+                print("|", end=" ")
+            if plateau[i][j] == 1:
+                print("X", end=" ")
+            elif plateau[i][j] == -1:
+                print("O", end=" ")
+            else:
+                print("·", end=" ")
+        print()
